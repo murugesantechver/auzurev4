6d138fdb05a31e8d4cf2e2955bcbb50a6473cad4
quilotechver <quilosoman07@gmail.com>
2024-05-09 16:54:27 +0530

    hytutyu

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
 // app.ts
 import express, { Request, Response } from "express";
+import morgan from "morgan";
 const app = express();
 
+// app.use(morgan("combined"));
+
+console.log("app>>>>>>>>");
 app.get("/api/user", (req: Request, res: Response) => {
   res.send("Hello from Express!");
 });
--- src/functions/httpTrigger1.ts
+++ src/functions/httpTrigger1.ts
@@ -1,20 +1,15 @@
 // index.js
-import { app } from "@azure/functions";
-import azureFunctionHandler from "azure-aws-serverless-express";
-import expressApp from "../../app";
+// import { app } from "@azure/functions";
+// import azureFunctionHandler from "azure-aws-serverless-express";
+// import expressApp from "../../app";
 
-app.http("httpTrigger1", {
-  methods: ["GET"],
-  route: "api/{*segments}",
-  handler: async (context, request) => {
-    console.log("context ::::: ", context);
-    console.log("request :::::: ", request);
+// app.http("httpTrigger1", {
+//   methods: ["GET"],
+//   route: "{*segments}",
+//   handler: azureFunctionHandler(expressApp),
+// });
 
-    return { body: `Hello` };
-  },
-});
-
-module.exports = azureFunctionHandler(expressApp);
+// export default app;
 /*
 // index.js
 const { app } = require("@azure/functions");
@@ -39,3 +34,28 @@ export default app.http("httpTrigger1", {
 });
 
 */
+import { InvocationContext, HttpRequest } from "@azure/functions";
+import { app } from "@azure/functions";
+import { createAzureFunctionHandler } from "azure-function-express";
+import azureFunctionHandler from "azure-aws-serverless-express";
+import serverlessExpress from "@codegenie/serverless-express";
+
+import expressApp from "../../app";
+
+const azureHandler = createAzureFunctionHandler(expressApp);
+const cachedServerlessExpress = serverlessExpress({ app: expressApp });
+
+const httpTrigger: any = async function (
+  req: HttpRequest,
+  context: InvocationContext
+): Promise<void> {
+  cachedServerlessExpress(expressApp)(context, req);
+  //cachedServerlessExpress(context, req);
+};
+
+export default app.http("httpTrigger1", {
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  route: "{*segments}",
+  authLevel: "anonymous",
+  handler: httpTrigger,
+});
